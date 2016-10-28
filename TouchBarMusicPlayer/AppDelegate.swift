8277ac5d32b50098332b9a3dbad6dd203a95d775
liupeng <liupeng@hujiang.com>
2016-10-29 02:12:30 +0800

    Initial Commit

---
+++ TouchBarMusicPlayer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TouchBarMusicPlayer
+//
+//  Created by liupeng on 29/10/2016.
+//  Copyright © 2016 liupeng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
